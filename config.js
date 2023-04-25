abfbbb2e50d70869ecf248cdaa724dcedb27f58d
lapybae <117326050+lapybae@users.noreply.github.com>
2023-04-25 22:45:39 +0700

    Add files via upload
    creat card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Mực(^)';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100068251609835';
+const eventName = 'Chúc Mừng sinh nhật';
+const titleCard = 'Đỗ C3 nè:))';
+const contentCard = ' Happy Birthday to Mực! Chúc đứa em gái một ngày sinh nhật tràn ngập tiếng cười và niềm vui. Wish all the good will come to you!!Send: BEST BROTHER';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = '1.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
